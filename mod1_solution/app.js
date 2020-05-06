c9cc830e05dfbf299e5fc4cb6196006c8f33b2e2
irinazhyzhyian <56150336+irinazhyzhyian@users.noreply.github.com>
2020-05-06 16:03:57 +0300

    First assignment

---
+++ mod1_solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+    
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.foodList = "";
+      $scope.message = "";
+    
+      $scope.sayMessage = function () {
+        if($scope.foodList==""){
+            $scope.message = "Please enter data first";
+        } 
+        else {
+            var list = $scope.foodList.split(',');
+            var listCount = 0;
+            for(var i=0; i<list.length; ++i){
+                if(list[i].replace(/\s+/g, '')!=""){
+                    listCount++;
+                }
+            }
+            if(listCount > 3)
+                $scope.message = "Too much!";
+            else
+                $scope.message = "Enjoy!";
+        }
+      };
+    }
+    })();
